7cc635e370fb3762cc6a02e6d9d5092bfe883431
nabadger <nbadger@mintel.com>
2018-09-29 12:18:41 +0100

    Fix typo

--- post-configuration.sh
+++ post-configuration.sh
@@ -7,8 +7,7 @@ echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
 sudo rmmod pcspkr
 
 ### Setup wallpapers
-mkdir -p $HOME/Pictures/wallpapers && cp ./wallpapers/* $HOMR/Pictures/wallpapers
+mkdir -p $HOME/Pictures/wallpapers && cp ./wallpapers/* $HOME/Pictures/wallpapers
 
 ### Copy over any customized etc configuration
 sudo cp etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
-
