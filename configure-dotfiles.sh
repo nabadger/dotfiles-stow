db0ec4ca7b116dd25fb00c368f5d08fb09a24746
Nick <nbadger@mintel.com>
2018-10-23 18:12:04 +0100

    Adding zsh-plugins

--- configure-dotfiles.sh
+++ configure-dotfiles.sh
@@ -3,3 +3,6 @@
 printf "Configuring dotfile symlinks"
 
 stow -t ${HOME} config dmenu gnupg profile screenlayout xresources wallpapers oh-my-zsh zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
